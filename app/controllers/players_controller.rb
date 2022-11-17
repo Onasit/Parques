970f4db9f47e08a5b7bd4bd3ce5ee2711b9e4afe
Tisana23 <camilamejor2312@gmail.com>
2022-11-17 13:43:22 -0500

    Se añadio las vistas de cada Player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,17 @@
 class PlayersController < ApplicationController
+    before_action :set_player, only: [:show]
+
+
     def index
         @players = Player.all
     end
+
+    def show
+    end
+
+    private
+    def set_player
+        @player = Player.find(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rails.application.routes.draw do
     sign_out: "logout",
     sign_up: "register"
   }
-
+  resources :players
   resources :games
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 #root to: "home#index"
 
 root to: "players#index"
-get "index", to: 'players#index'
+# get "index", to: 'players#index'
+
+# get "show", to: 'players#show'
 
 end
