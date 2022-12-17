70e96289ae4154e145f4cb376d442003a462de9d
Cristian Joly <c-joly05@hotmail.com>
2022-12-17 13:12:35 -0500

    Poder eliminar deaths eliminando games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,12 @@
 class Game < ApplicationRecord
   
-  has_many :player_games
+  has_many :player_games, dependent: :destroy
   has_many  :players, through: :player_games
-  has_many :deaths
+  has_many :deaths, dependent: :destroy
+
+
+
+
   
   accepts_nested_attributes_for :player_games, allow_destroy: true
 
